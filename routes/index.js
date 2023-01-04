614b48becb8bb543de0191a1f799634cbefc428f
Lucas-Biolatti <lucasbiolatti@gmail.com>
2023-01-04 12:16:20 -0300

    primer comit

---
+++ routes/index.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+var conexion = require('../db/db')
+var session = require('express-session')
+
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+	if(req.session.loggedin){
+		res.send("Usted ya esta Logueado")
+	}else{
+		res.render('login');
+	}
+  
+});
+
+router.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		conexion.query('SELECT * FROM user WHERE name = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				request.session.rol = results[0].rol;
+				request.session.nombre = results[0].nombre;
+				request.session.apellido = results[0].apellido;
+				
+				
+				// Redirect to home page
+				response.redirect('/');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+router.get('/', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		
+		response.render('./'+request.session.rol+'/index',{nombre:`${request.session.apellido}, ${request.session.nombre}`});
+		
+		}
+					else {
+					// Not logged in
+					response.render('login');
+						}
+					response.end();
+});
+router.get('/logout',async (req,res)=>{
+	await req.session.destroy();
+	await res.redirect('/login');
+	
+})
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,212 @@
+var express = require('express');
+const url = require('url');
+var router = express.Router();
+var conexion = require('../db/db')
+/* GET users listing. */
+function fecha(x){
+  let f = new Date(x);
+  let fecha = f.getDate()+"/"+f.getMonth()+"/"+f.getUTCFullYear();
+  return fecha;
+}
+//MANTENIMIENTO
+router.get('/mtto:?',(req,res)=>{
+  //lISTAR SECTORES Y REDIRIGIR
+  if (req.session.loggedin) {
+    let sql1 = "SELECT * FROM sector";
+    let sector = [];
+    let mensaje = req.query.mensaje
+    
+    conexion.query(sql1,async (error,result,files)=>{
+        if(!error){
+           
+            for(let i=0;i<result.length;i++){
+                sector.push(result[i])
+            }
+         
+        }
+    
+      await res.render("./users/mantenimiento/mtto",{sector:sector,
+        nombre:`${req.session.apellido}, ${req.session.nombre}`,
+      mensaje:mensaje});   
+      console.log(mensaje) 
+    })
+  }else{
+    res.render('login')
+  }
+})
+router.get('/ordenMtto',(req,res)=>{
+  if (req.session.loggedin) {
+    let idSector = url.parse(req.url,true).query.id;
+    let sector = url.parse(req.url,true).query.nombre;
+    let sql2 = "SELECT * FROM equipo WHERE Sector=?";
+    let equipo = [];
+    conexion.query(sql2,[parseInt(idSector)],(error,result,files)=>{
+        if(!error){
+           
+            for(let i=0;i<result.length;i++){
+                equipo.push(result[i])
+            }
+            res.render('./users/mantenimiento/agregar',{equipo:equipo,idSector:idSector,sector:sector,nombre:`${req.session.apellido}, ${req.session.nombre}`});
+        }
+  
+    })
+    
+  } else {
+    res.render('login');
+  }
+  
+})
+router.post('/ordenMtto',(req,res)=>{
+  if (req.session.loggedin) {
+    let sql = "INSERT INTO `ordentrabajo`(`detecto`,`avisar`,`tel`, `sector`, `equipo`, `fecha`, `turno`, `paradaProceso`, `prioridad`, `tipoParada`, `horaInicio`, `horaFin`, `descripcion`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    conexion.query(sql,[
+    req.body.detecto,
+    req.body.avisar,
+    req.body.tel,
+    req.body.sector,
+    req.body.equipo,
+    req.body.fecha,
+    req.body.turno,
+    req.body.paradaProceso,
+    req.body.prioridad,
+    req.body.tipoParada,
+    req.body.horaInicio,
+    req.body.horaFin,
+    req.body.descripcion],(error,fields)=>{
+      if(!error){
+        res.redirect('mtto?mensaje=Se agrego exitosamente la OT');
+      }else{
+        res.redirect('mtto?mensaje=Error al intentar agregar OT');
+    }})
+  } else {
+    res.render('login');
+  }
+})
+
+
+//SYSO
+router.get('/syso', (req,res)=>{
+  if (req.session.loggedin) {
+    let sql1 = "SELECT * FROM sector";
+    let sql2= "SELECT * FROM accidentes";
+    let sql3= "SELECT * FROM actosinseguros";
+    let sector = [];
+    let accidentes=0;
+    let actos=0;
+
+    conexion.query(sql2,(error,result)=>{
+        if(!error){
+            for(let i=0;i<result.length;i++){
+                accidentes++
+            }
+            
+        }
+    })
+   conexion.query(sql3,(error,result)=>{
+        if(!error){
+        for(let i=0;i<result.length;i++){
+            actos++
+        }
+    }
+    })
+
+    
+   conexion.query(sql1,(error,result,files)=>{
+        if(!error){
+            
+            for(let i=0;i<result.length;i++){
+                sector.push(result[i])
+            }
+         
+        }
+    
+    res.render("./users/syso/index",{
+      sector:sector,
+      accidentes:accidentes,
+      actos:actos,
+      nombre:`${req.session.apellido}, ${req.session.nombre}`});    
+    })
+  }else{
+    res.render('login')
+  }
+})
+
+// AUTONOMO
+router.get('/autonomo',(req,res)=>{
+  if (req.session.loggedin) {
+    res.render('./users/autonomo/index',{
+      nombre:`${req.session.apellido}, ${req.session.nombre}`,
+    })
+  }else{
+    res.render('login')
+  }
+})
+
+//DATOS PARA USAR CON FETCH
+router.get('/items',(req,res)=>{
+  const sql = "SELECT * FROM items WHERE 1"
+  conexion.query(sql,(error,result)=>{
+      if(!error){
+          res.send(result);
+          
+      }
+      
+  })
+})
+router.get('/sector',(req,res)=>{
+  let sql1 = "SELECT * FROM sector";
+  conexion.query(sql1,(error,result,files)=>{
+      if(!error){
+         res.send(result);
+      }else{
+          alert("No se pudo obtener los sectores");
+      }
+  })
+})
+router.get('/nomina',(req,res)=>{
+  let sql1 = "SELECT * FROM nomina";
+  conexion.query(sql1,(error,result,files)=>{
+      if(!error){
+         res.send(result);
+      }else{
+          alert("No se pudo obtener la nomina");
+      }
+  })
+})
+
+router.get('/ordenes',(req,res)=>{
+  const sql="SELECT * FROM ordentrabajo WHERE NOT estado='cerrado'"
+  conexion.query(sql,(error,fields)=>{
+      if(!error){ 
+          let data=[];
+          for (let i = 0; i < fields.length; i++) {
+              let dato = {
+                  idOrden:fields[i].idOrden,
+                  sector:fields[i].sector,
+                  avisar:fields[i].avisar,
+                  equipo:fields[i].equipo,
+                  fecha:fecha(fields[i].fecha),
+                  turno:fields[i].turno,
+                  descripcion:fields[i].descripcion,
+                  estado:fields[i].estado,
+                  accion:'<a href="/resolverOrden?idOrden='+fields[i].idOrden+'">Cerrar</a>'
+
+              }
+              data.push(dato)
+              
+          }
+          
+          res.send(data);
+      }
+      else console.log("Error al conectar con BD OT")
+  })
+})
+router.get('/tarjetas',(req,res)=>{
+  const sql="select * from tarjetasAm where 1"
+  conexion.query(sql,(error,fields)=>{
+      if (!error) {
+          res.send(fields);
+      }
+  })
+})
+module.exports = router;
