f30c203ca7f6498b27995d463008ca0067f2485a
Lucas-Biolatti <lucasbiolatti@gmail.com>
2023-07-10 23:09:43 -0300

    creacion de parte Admin

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var SeguridadRouter = require('./routes/Seguridad');
 var MantenimientoRouter = require('./routes/Mantenimiento');
+var AdminRouter = require('./routes/Admin');
 var app = express();
 
 // view engine setup
@@ -36,6 +37,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Seguridad',SeguridadRouter);
 app.use('/Mantenimiento',MantenimientoRouter);
+app.use('/Admin',AdminRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
