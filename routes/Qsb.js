c11f28b0636fb4c47450aa45b8727599efcdb71d
Lucas-Biolatti <lucasbiolatti@gmail.com>
2024-03-27 17:41:07 -0300

    2703

---
+++ routes/Qsb.js
@@ -0,0 +1,33 @@
+var express = require('express');
+const url = require('url');
+var router = express.Router();
+var conexion = require('../db/db');
+
+
+function fecha(x){
+  let f = new Date(x);
+  let fecha = f.getDate()+"/"+f.getMonth()+"/"+f.getUTCFullYear();
+  return fecha;
+};
+function fechaEdit(x){
+  let dia = (x.getUTCDate()<10?'0':'')+x.getUTCDate();
+  let mes = ((x.getMonth()+1)<10?'0':'')+(x.getMonth()+1);
+  let f = x.getUTCFullYear()+"-"+mes+"-"+dia;
+  return f;
+}
+
+
+
+//const nombre = `${req.session.apellido}, ${req.session.nombre}`
+//MANTENIMIENTO
+router.get('/',(req,res)=>{
+    if (req.session.loggedin && req.session.rol=="Qsb") {
+        res.render('./Qsb/index')
+    
+    } else {
+      res.render('login',{
+        mensaje:`No esta logeado o no tiene autorizacion para este sitio. Verifique sus credenciales`});
+    }
+})
+
+module.exports = router;
