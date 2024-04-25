42221b7c488e6a9b0fad05422c77fba722dc1c6b
Lucas-Biolatti <lucasbiolatti@gmail.com>
2024-04-25 17:28:33 -0300

    250424

--- routes/Qsb.js
+++ routes/Qsb.js
@@ -62,20 +62,49 @@ router.post("/agregaraccion",(req,res)=>{
     let disponibilidad = req.body.disponibilidad;
     let disp_molde = req.body.disp_molde;
     let observaciones = req.body.observaciones;
-    const sql = `INSERT INTO indicadores (fecha,sector, accidentes, c_programa,scrap,disponibilidad,disp_molde, observaciones)VALUES (${fecha},"${sector}", ${accidentes}, ${c_programa},${scrap},${disponibilidad},${disp_molde}, "${observaciones}");`
+    let retrabajo = req.body.retrabajo;
+    let ret_laca = req.body.ret_laca;
+    let sc_laca = req.body.sc_laca;
+    let entregas = req.body.entregas;
+
+    const sql = `INSERT INTO indicadores (fecha,sector,accidentes,c_programa,retrabajo,scrap,disponibilidad,disp_molde,ret_laca,sc_laca,entregas,observaciones)VALUES (${fecha},"${sector}",${accidentes},${c_programa},${retrabajo},${scrap},${disponibilidad},${disp_molde},${ret_laca},${sc_laca},${entregas},"${observaciones}");`
     conexion.query(sql,(error,row)=>{
       if (!error) {
-        res.redirect(`/Qsb/iny?sector=Inyeccion/`)
+        res.redirect(`/Qsb/iny?sector=${sector}/`)
       }else{
         res.send(error)
       }
     })
    
 
-} else {
-  res.render('login',{
-    mensaje:`No esta logeado o no tiene autorizacion para este sitio. Verifique sus credenciales`});
-}
+    } else {
+        res.render('login',{
+          mensaje:`No esta logeado o no tiene autorizacion para este sitio. Verifique sus credenciales`});
+      }
+  });
+router.post('/actionplan',(req,res)=>{
+  if (req.session.loggedin && req.session.rol=="Qsb") {
+    let fecha = req.body.fecha;
+    let sector = req.body.sector;
+    let descripcion = req.body.descripcion;
+    let accion = req.body.accion;
+    let responsable = req.body.responsable;
+    let comentario = req.body.comentario;
+    let estado = req.body.estado;
+    let sector1 = req.body.sector1;
+
+    const sql = `INSERT INTO planAccionQsb (fecha,sector,descripcion,accion,responsable,comentario,estado) VALUES (${fecha},"${sector}","${descripcion}","${accion}","${responsable}","${comentario}","${estado}");`
+    conexion.query(sql,(error,rows)=>{
+      if (!error) {
+        res.redirect(`/Qsb/iny?sector=${sector1}/`)
+      }else{
+        res.send(error)
+      }
+    })
+  } else {
+    res.render('login',{
+      mensaje:`No esta logeado o no tiene autorizacion para este sitio. Verifique sus credenciales`});
+  }
 })
 
 module.exports = router;
